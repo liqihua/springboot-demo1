5b0b28aef8ec6e6a33d4c8efdf6e014d2734fdd6
liqihua <iamliqihua@qq.com>
2018-01-18 14:38:16 +0800

    liqihua

--- src/main/java/com/liqihua/Run.java
+++ src/main/java/com/liqihua/Run.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages={"com.*"})//如果不加这个注解，只会扫描本类同级和下级往下的目录里面的controller
 public class Run {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(Run.class,args);
     }

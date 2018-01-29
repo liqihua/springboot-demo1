5abb112f9360f860f18b00f453c88698ae63785e
liqihua <iamliqihua@qq.com>
2018-01-29 16:07:26 +0800

    liqihua

---
+++ docker/restart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "----- killing pid ..."
+pidlist=`ps -ef|grep 9111|grep -v "grep"|awk '{print $2}'`
+if [ "$pidlist" = "" ]
+   then
+       echo "----- no pid alive!"
+else
+  echo "----- pid list :$pidlist"
+  kill -9 $pidlist
+  echo "----- KILL $pidlist:"
+  echo "----- service stop success"
+fi
+echo "----- starting project ..."
+nohup java -jar -Dserver.port=9111 springboot-demo1.war &
+echo "---command finished."
